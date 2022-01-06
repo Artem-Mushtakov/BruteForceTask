4ace4e16d8e4f59ffa9c31d69a99716962765795
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-01-06 22:50:20 +0400

    Add enum password selection state.

---
+++ BruteForceTask/Sources/Module/PasswordSelectionState.swift
@@ -0,0 +1,14 @@
+//
+//  PasswordSelectionState.swift
+//  BruteForceTask
+//
+//  Created by Артем on 06.01.2022.
+//
+
+import Foundation
+
+enum PasswordSelectionState {
+    case start
+    case search
+    case complete
+}
