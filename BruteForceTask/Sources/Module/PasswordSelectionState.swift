cf470ea5e9e9685666449429d2b112bfc8c809d2
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-01-08 10:51:17 +0400

    Add reference comment PasswordSelectionState.

--- BruteForceTask/Sources/Module/PasswordSelectionState.swift
+++ BruteForceTask/Sources/Module/PasswordSelectionState.swift
@@ -7,6 +7,14 @@
 
 import Foundation
 
+/**
+ Этот enum используется для различной настройки Ui элементов в зависимости от состояния подбора пароля.
+ - start: этот кейс используется при генерации пароля.
+ - search: этот кейс используется при подборе пароля.
+ - complete: этот кейс используется при успешном подборе пароля.
+ - Authors: Mushtakov Artem, email: a.vladimirovich@internet.ru
+ */
+
 enum PasswordSelectionState {
     case start
     case search
